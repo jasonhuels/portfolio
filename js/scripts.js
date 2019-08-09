52bdbc64318574388e4770236d2b78e69e46efb5
Jason Huels <jasonhuels@yahoo.com>
2019-08-09 14:49:14 -0700

    add content warning to gif, fix typo

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-// var myElement = document.getElementById("bridget");
-//
-// myElement.onmouseover = function() {mouseOver()};
-// myElement.onmouseout = function() {mouseOut()};
-
+/**
+* Increase the width of HTML element during onmouseenter event
+*/
 function mouseOver(myElement, size) {
-  myElement.setAttribute("style", "width: " + size + "%;");
+  myElement.setAttribute("style", "opacity: 1; width: " + size + "%;");
 }
 
+/**
+* Decrease the width of HTML element during onmouseleave event
+*/
 function mouseOut(myElement, size) {
-  myElement.setAttribute("style", "width: " + size +"%;");
+  myElement.setAttribute("style", "opacity: 1; width: " + size +"%;");
 }
